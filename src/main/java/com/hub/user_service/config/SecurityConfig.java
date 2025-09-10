3adda399dabd64b744c132ffc55985daf8c9cd2c
phccnguyenn <phccnguyenn2554@gmail.com>
2025-09-10 09:57:45 +0700

    Update for FE

--- src/main/java/com/hub/user_service/config/SecurityConfig.java
+++ src/main/java/com/hub/user_service/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collection;
 import java.util.Map;
